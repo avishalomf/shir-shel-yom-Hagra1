5cd2b965ad284b2a5efd669f41e97b35bcebff37
avishalomf <156226805+avishalomf@users.noreply.github.com>
2026-04-06 20:48:05 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'shir-shel-yom-v1';
+const ASSETS = [
+  './index.html',
+  './psalms.json',
+  './app.webmanifest',
+  './icon-192.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
