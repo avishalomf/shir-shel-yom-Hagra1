5ef76830d25b5f5566b856bf3e314a7b9f3e0494
avishalomf <156226805+avishalomf@users.noreply.github.com>
2026-04-06 21:00:42 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,47 @@
+// VERSION: 1.0.0 | PROJECT: Shir Shel Yom PWA | SW
 const CACHE_NAME = 'shir-shel-yom-v1';
+
+// רשימת הקבצים לשמירה במטמון
 const ASSETS = [
+  './',
   './index.html',
   './psalms.json',
   './app.webmanifest',
+  './favicon.png',
   './icon-192.png'
 ];
 
+// התקנה - שמירת הקבצים בזיכרון
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('SW: Caching Assets');
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
+// הפעלה - ניקוי זיכרון ישן אם קיים
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log('SW: Clearing Old Cache');
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// שליפת נתונים - עדיפות למטמון (Offline First)
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
 });
