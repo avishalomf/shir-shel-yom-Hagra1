106c355e59773060aafa10322042c485e1151a5e
avishalomf <156226805+avishalomf@users.noreply.github.com>
2026-04-07 07:29:22 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,15 @@
-const CACHE_NAME = 'shir-shel-yom-v2';
+const CACHE_NAME = 'shir-shel-yom-v1.3.2';
 const ASSETS = [
-  '/shir-shel-yom-Hagra1/',
-  '/shir-shel-yom-Hagra1/index.html',
-  '/shir-shel-yom-Hagra1/manifest.json',
-  '/shir-shel-yom-Hagra1/psalms.json',
-  '/shir-shel-yom-Hagra1/favicon.png',
-  '/shir-shel-yom-Hagra1/icon-192.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './psalms.json',
+  './favicon.png',
+  './icon-192.png'
 ];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
